37ef19ab584e25137dffe39171fc8908288c1a88
chustert <christianhustert@gmail.com>
2023-05-09 11:36:56 +1200

    new file:   ch-wf-resize-components.js

---
+++ ch-wf-resize-components.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  console.log("View height on load:", window.innerHeight);
+  console.log("View width on load:", window.innerWidth);
+});
+
+window.addEventListener("resize", () => {
+  console.log("View height on resize:", window.innerHeight);
+  console.log("View height on resize:", window.innerWidth);
+});
